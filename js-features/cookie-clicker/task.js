89de601df724ca660cdc2aa2f5e8feec50473f57
AquaDll <aquadefin@gmail.com>
2021-11-28 23:35:47 +0300

    issue 1

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,18 +4,22 @@ const clickSpeed = document.getElementById('click__speed')
 let count = 0;
 let plus = 220;
 let minus = 200;
+let timeFirst, timeSecond;
 // Скорость клика
 const time = new Date()
 let seconds = time.getSeconds()
 cookie.onclick = function() {
     //Счетчик
     count +=1;
+    timeFirst = new Date().getTime()
     clicksAdd.innerHTML = count;
     if (cookie.width === minus) {
         cookie.width = plus;
     } else {
         cookie.width = minus;
+        timeSecond = new Date().getTime()
     }
+    clickSpeed.textContent = 1 / ((timeFirst - timeSecond) / 1000);
     // Тут что-то еще будет...
 }
 
