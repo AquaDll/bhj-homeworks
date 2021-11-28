bf0261897ec630d5223cda52b31ab99d8a94449f
AquaDll <aquadefin@gmail.com>
2021-11-28 22:42:32 +0300

    timer + windows.location()

---
+++ js-features/countdown/task.js
@@ -0,0 +1,13 @@
+let count = 59;
+const timer = document.getElementById('timer');
+const tick = () => {
+  timer.innerHTML = count--
+  if (count !== 0) {
+    setTimeout(tick, 1000)
+  } else {
+    alert(location ="https://yt3.ggpht.com/a/AATXAJz7aj99oBJJrdxJXUZkfD-wCkBNaCIS4ee3mNwu=s900-c-k-c0x00ffffff-no-rj")
+  }
+
+}
+tick()
+
