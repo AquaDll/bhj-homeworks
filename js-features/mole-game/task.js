d6433aa25710a21505d47e8cc4afc853f5f78a02
AquaDll <aquadefin@gmail.com>
2021-11-29 00:18:04 +0300

    task 3 fix

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,26 +1,26 @@
 function getHole(index) {
     return document.getElementById(`hole${index}`)
 }
+
+let stock = 0;
 const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
-
 for (let i = 1; i < 10; i++) {
     getHole(i).onclick = () => {
         if (getHole(i).className == 'hole hole_has-mole') {
             dead.textContent++
             if (dead.textContent == 10) {
                 alert('Победа =)')
-                dead.textContent = 0
-                lost.textContent = 0
+                dead.textContent = stock;
+                lost.textContent = stock;
             }
-        } else  {
+        } else {
             lost.textContent++
-             if (lost.textContent == 5) {
-                 alert('Вы проиграли =(')
-                dead.textContent = 0
-                lost.textContent = 0
-             }
-
+            if (lost.textContent == 5) {
+                alert('Вы проиграли =(')
+                dead.textContent = stock;
+                lost.textContent = stock;
+            }
         }
     }
 }
