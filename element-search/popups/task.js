bed2b8951fb6f7ed2e2206dadacc5f9106dda30c
AquaDll <aquadefin@gmail.com>
2021-12-05 22:11:43 +0300

    1 try

---
+++ element-search/popups/task.js
@@ -0,0 +1,24 @@
+const modal_main = document.getElementById('modal_main')
+modal_main.classList.add('modal_active')
+
+// close
+const modal__close = document.getElementsByClassName('modal__close');
+const modal_success = document.getElementById('modal_success');
+for (let i = 0; i < modal__close.length; i++) {
+    modal__close.item(i).onclick = () => {
+        modal_main.classList.remove('modal_active');
+        modal_success.classList.remove('modal_active');
+    };
+}
+// Красный -> Зеленый
+let links = document.getElementsByTagName("a");
+let arr = Array.from(links)
+console.log(arr)
+let closeElement = links.item(1)
+console.log(closeElement)
+closeElement.onclick = () => {
+    closeElement.classList.add('btn_success')
+    closeElement.innerHTML = 'Хорошо сделано!'
+}
+
+
