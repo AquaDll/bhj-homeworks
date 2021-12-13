f07efceeb22305a6a82dd0b3f94855d5d7db1f68
AquaDll <aquadefin@gmail.com>
2021-12-13 13:21:58 +0300

    fix

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,11 +4,9 @@ menu__link.forEach(element => {
     element.onclick = function () {
          const menu_active = document.querySelector(".menu_active");
 
-         if (menu_active) {
-        if (menu_active !== element.nextElementSibling) {
+         if (menu_active && menu_active !== element.nextElementSibling) {
             menu_active.classList.remove('menu_active')
         }
-    }
          element.nextElementSibling.classList.toggle('menu_active')
          return false;
     }
